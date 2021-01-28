b8a26b748efef924f25bc2e1469a05a57c8e0ed1
Halee Pagel <halee.pagel@gmail.com>
2021-01-28 12:27:10 +0900

    created first build folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import angelsBaseball from "./images/angelsBaseball.jpg";
+import "./App.css";
+/**
+ * LINK TO MLB API FOR 2021
+ * https://statsapi.mlb.com/api/v1/schedule?sportId=1&startDate=02/01/2021&endDate=11/30/2021
+ */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={angelsBaseball} className="App-logo" alt="logo" />
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.mlb.com/angels"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          angels baseball website
         </a>
       </header>
     </div>
